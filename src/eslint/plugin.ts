07c0c9245c2b9296b5b603d8ea422a7145543e98
Mark Carpenter <info@aireinvestor.com>
2025-04-08 09:25:59 -0500

    fix: Remove development files from web branch

--- src/eslint/plugin.ts
+++
@@ -1,6 +0,0 @@
-export const customPlugin = {
-  rules: {
-    'single-types-file': singleTypesFileRule,
-    'require-type-declarations': requireTypeDeclarationsRule,
-  },
-};
--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import env from './config/env';
-
-// Export all modules
-export * from './config/env';
-export * from './scripts/merge';
-
-// Add more exports as we create more modules
